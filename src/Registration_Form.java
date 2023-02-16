fc4ed86279f4ca9b7df2a58e72ab7f6f6c4ab608
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 20:02:21 +0000

    create Registration form interfaces

---
+++ src/Registration_Form.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Registration_Form {
+    private JPanel panelMain;
+    private JLabel lbl_UserReg;
+    private JLabel lbl_FullName;
+    private JLabel lbl_Position;
+    private JLabel lbl_UserName;
+    private JLabel lbl_Password;
+    private JLabel lbl_ConfPw;
+    private JButton btn_Register;
+    private JButton btn_Cansel;
+    private JTextField textField1;
+    private JRadioButton btn_radioManager;
+    private JRadioButton btn_radioClerk;
+    private JTextField txt_UserName;
+    private JPasswordField txt_Password;
+    private JPasswordField txt_ConfPw;
+}
