7a28024a0d7ce1863936e88b6e1b93fe02bf1ea2
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 19:53:48 +0000

    create add room form interfaces

---
+++ src/AddRoom_Form.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class AddRoom_Form {
+    private JPanel panelMain;
+    private JLabel lbl_AddRoom;
+    private JLabel lbl_RoomNo;
+    private JLabel lbl_RoomType;
+    private JLabel lbl_RoomSize;
+    private JButton btn_Save;
+    private JButton btn_Cancel;
+    private JTextField txt_RoomNo;
+    private JTextField txt_RoomSize;
+    private JRadioButton btn_radioComputLab;
+    private JRadioButton btn_radioTutorial;
+    private JRadioButton btn_radioLecture_Theatre;
+}
---
+++ src/BookingConfirmation.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class BookingConfirmation {
+    private JPanel panelMain;
+    private JLabel lbl_RoomNo;
+    private JLabel lbl_RoomType;
+    private JLabel lbl_BookingDate;
+    private JLabel lbl_BookingTime;
+    private JButton btn_Save;
+    private JLabel lbl_Note;
+    private JLabel lbl_ContactNo;
+    private JLabel lbl_ClientName;
+    private JLabel lbl_Occasion;
+    private JButton cancelButton;
+    private JLabel lbl_ShowRoomNo;
+    private JLabel lbl_ShoeRoomType;
+    private JComboBox comboBox1;
+    private JTextField txt_ClientName;
+    private JTextField txt_ClientContactNo;
+    private JTextArea txt_SpecialNotes;
+}
