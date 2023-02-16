6364c74942d565a647722200312c6dcd2567417e
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 20:02:53 +0000

    create Update form interfaces

---
+++ src/UpdateRoom_Form.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class UpdateRoom_Form {
+    private JPanel panelMain;
+    private JLabel lbl_UpdateRoom;
+    private JLabel lbl_RoomNo;
+    private JButton btn_Save;
+    private JButton btn_Cancel;
+    private JLabel lbl_ShowRoomNo;
+    private JComboBox comboBox1;
+    private JLabel lbl_Reason;
+    private JTextArea txt_Reason;
+}
