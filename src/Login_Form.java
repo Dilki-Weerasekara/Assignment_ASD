de89cf64e1f4fc208bd35a7a9b8bc79d57b9cba1
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 20:01:18 +0000

    create login form interfaces

---
+++ src/Login_Form.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Login_Form {
+
+    private JLabel lbl_Login;
+        private JLabel lbl_Username;
+        private JLabel lbl_Password;
+        private JButton btn_cansel;
+        private JFormattedTextField dontHaveAnAccountFormattedTextField;
+        private JTextField txt_Username;
+        private JPasswordField txt_password;
+        private JLabel lbl_LogoIcon;
+        private JButton btn_Logic;
+
+    }
+
