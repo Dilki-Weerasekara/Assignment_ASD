29630c8a95f62e1911433f99c9d43d7a9e60e951
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 20:05:05 +0000

    create View Room Manager form interfaces

---
+++ src/ViewRoom_Manager.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ViewRoom_Manager {
+    private JLabel lbl_RoomDetails;
+    private JTextField txt_Search;
+    private JTable tb_ViewRoomDetails;
+    private JPanel panelMain;
+    private JLabel lbl_searchIcon;
+}
