6585b5f98b30c8ffcce7d3d870a73da65cffe63d
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 20:04:45 +0000

    create View Room clerk form interfaces

---
+++ src/ViewRoom_Clerk.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ViewRoom_Clerk {
+    private JLabel lbl_ViewRoom;
+    private JPanel panelMain;
+    private JTextField txt_Search;
+    private JLabel lbl_Search;
+    private JTable tb_ViewRoomDetails;
+}
