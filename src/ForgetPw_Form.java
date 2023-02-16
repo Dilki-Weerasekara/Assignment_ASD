141eeb5aea6eb0dee60e011421be9b03ef859186
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 19:54:29 +0000

    create forget password form interfaces

---
+++ src/ForgetPw_Form.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ForgetPw_Form {
+    private JPanel panelMain;
+    private JLabel lbl_forgetPw;
+    private JLabel lbl_currentPw;
+    private JLabel lbl_NewPw;
+    private JButton btn_Update;
+    private JButton btn_cansel;
+    private JPasswordField txt_CurrentPw;
+    private JPasswordField txt_NewPw;
+}
