b3ae82ebe70d6a01326ab44ba2c861e657d60359
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-16 20:03:24 +0000

    create View Booking form interfaces

---
+++ src/ViewBooking_Details.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ViewBooking_Details {
+    private JPanel panelMain;
+    private JTextField txtSearch;
+    private JLabel lbl_Search;
+    private JTable tb_ViewBookingDetails;
+}
