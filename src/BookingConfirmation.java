66eaaf0157befbb551502f775715cd6ada0e990f
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 00:46:43 +0000

    Minor changes in Booking Confirmation GUI

--- src/BookingConfirmation.java
+++ src/BookingConfirmation.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 
+
 public class BookingConfirmation {
     private JPanel panelMain;
     private JLabel lbl_RoomNo;
-    private JLabel lbl_RoomType;
     private JLabel lbl_BookingDate;
     private JLabel lbl_BookingTime;
     private JButton btn_Save;
@@ -14,8 +14,12 @@ public class BookingConfirmation {
     private JButton cancelButton;
     private JLabel lbl_ShowRoomNo;
     private JLabel lbl_ShoeRoomType;
-    private JComboBox comboBox1;
+    private JComboBox comboBox_occasion;
     private JTextField txt_ClientName;
     private JTextField txt_ClientContactNo;
     private JTextArea txt_SpecialNotes;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JLabel lbl_RoomType;
+    private JLabel lbl_FromDate;
 }
