406528652d38943056b0db5ae4114a065ab5c786
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 16:13:08 +0000

    Create new GUI for Manager to View Remove Room Details

---
+++ src/RemoveRoom_Details.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class RemoveRoom_Details {
+    private JPanel panelMain;
+    private JLabel lbl_RemoveRoomDetails;
+    private JTextField txt_Search;
+    private JLabel lbl_SearchIcon;
+    private JTable tb_RemoveRoom;
+}
