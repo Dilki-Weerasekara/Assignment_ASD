a65cefb9241482c73bb78e3818ebc1905b639c26
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 05:11:33 +0000

    Redesign the layout of Forget Password Form

--- src/ForgetPw_Form.java
+++ src/ForgetPw_Form.java
@@ -3,10 +3,12 @@ import javax.swing.*;
 public class ForgetPw_Form {
     private JPanel panelMain;
     private JLabel lbl_forgetPw;
-    private JLabel lbl_currentPw;
-    private JLabel lbl_NewPw;
+    private JLabel lbl_Email;
+    private JLabel lbl_ConfirmPw;
     private JButton btn_Update;
     private JButton btn_cansel;
-    private JPasswordField txt_CurrentPw;
+    private JPasswordField txt_ConfirmPw;
+    private JTextField txt_Email;
     private JPasswordField txt_NewPw;
+    private JLabel lbl_NewPw;
 }
