93e77f45b1e9f1420e7b438252faeda8f38986e4
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 14:59:15 +0000

    Create new GUI for Manager to Update the Booking Details

---
+++ src/UpdateBooking_Manager.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class UpdateBooking_Manager {
+    private JPanel panelMain;
+    private JLabel lbl_UpdateBooking;
+    private JLabel lbl_RoomNo;
+    private JLabel lbl_Facilities;
+    private JLabel lbl_BookingDate;
+    private JLabel lbl_BookingTime;
+    private JLabel lbl_Occasion;
+    private JLabel lbl_ClientName;
+    private JLabel lbl_ClientContact;
+    private JLabel lbl_SpecialNotes;
+    private JComboBox comboBox_RoomNo;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+    private JButton btn_Update;
+    private JButton btn_Cancel;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JLabel lbl_DateFrom;
+    private JTextField txt_FromDate;
+    private JLabel lbl_DateTo;
+    private JTextField textField5;
+    private JLabel lbl_TimeStart;
+    private JTextField txt_TimeStart;
+    private JLabel lbl_TimeEnd;
+    private JTextField textField6;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+}
