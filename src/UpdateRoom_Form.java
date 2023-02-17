3615e4892a8db1bd4cde28ac611a881a57ae63b2
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 04:44:22 +0000

    New Component add to UpdateRoom_Form GUI

--- src/UpdateRoom_Form.java
+++ src/UpdateRoom_Form.java
@@ -4,10 +4,21 @@ public class UpdateRoom_Form {
     private JPanel panelMain;
     private JLabel lbl_UpdateRoom;
     private JLabel lbl_RoomNo;
-    private JButton btn_Save;
+    private JButton btn_Update;
     private JButton btn_Cancel;
     private JLabel lbl_ShowRoomNo;
     private JComboBox comboBox1;
     private JLabel lbl_Reason;
     private JTextArea txt_Reason;
+    private JComboBox comboBox2;
+    private JLabel lbl_DatePeriod;
+    private JLabel lbl_TimePeriod;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JLabel lbl_DateFrom;
+    private JLabel lbl_DateTo;
+    private JTextField textField4;
+    private JLabel lbl_TimeFrom;
+    private JLabel lbl_TimeTo;
 }
