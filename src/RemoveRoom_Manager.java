deae96558cd582f05d5e8689a216929dea1b4633
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 16:12:50 +0000

    Create new GUI for Manager to Remove Room Details

---
+++ src/RemoveRoom_Manager.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class RemoveRoom_Manager {
+    private JPanel panelMain;
+    private JLabel lbl_RemoveRoom;
+    private JLabel lbl_RoomNo;
+    private JComboBox comboBox_RoomNO;
+    private JLabel lbl_Reason;
+    private JTextArea txt_Reason;
+    private JButton btn_Delete;
+    private JButton btn_Cancel;
+}
