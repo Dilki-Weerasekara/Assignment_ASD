0fce7a621ad4711728900e7952bcc7791f23f364
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 04:45:16 +0000

    New SearchIcon add to ViewBooking_Details.form GUI

--- src/ViewBooking_Details.java
+++ src/ViewBooking_Details.java
@@ -3,6 +3,6 @@ import javax.swing.*;
 public class ViewBooking_Details {
     private JPanel panelMain;
     private JTextField txtSearch;
-    private JLabel lbl_Search;
     private JTable tb_ViewBookingDetails;
+    private JLabel lbl_SerachIcon;
 }
