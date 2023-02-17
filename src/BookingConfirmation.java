4ce4598345dd69d92dcb0a94cc5d520fbca40019
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 04:43:10 +0000

    Layout Changes in Booking confirmation GUI

--- src/BookingConfirmation.java
+++ src/BookingConfirmation.java
@@ -18,8 +18,19 @@ public class BookingConfirmation {
     private JTextField txt_ClientName;
     private JTextField txt_ClientContactNo;
     private JTextArea txt_SpecialNotes;
-    private JTextField textField2;
     private JComboBox comboBox1;
-    private JLabel lbl_RoomType;
+    private JLabel lbl_RoomFacilities;
     private JLabel lbl_FromDate;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JLabel lbl_ToDate;
+    private JLabel lbl_TimeStart;
+    private JLabel lbl_TimeEnd;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JComboBox comboBox2;
+    private JLabel lbl_RoomSize;
+    private JLabel lbl_ShowSize;
+    private JLabel lbl_RoomType;
+    private JLabel lbl_ShowType;
 }
