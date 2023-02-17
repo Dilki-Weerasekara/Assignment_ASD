7d86119510a58c6e62b70017bc24fa13d6ab67e8
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 21:25:13 +0000

    Change the Layout of Dashboard_Clerk.form

---
+++ src/Dashboard_Clerk.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Dashboard_Clerk {
+    private JPanel panelMain;
+    private JLabel lbl_DashboardClerk;
+    private JButton btn_ViewRooms;
+    private JButton btn_AddBooking;
+    private JButton btn_ViewBooking;
+    private JPanel panelButtons;
+    private JLabel lbl_BackgroundImage;
+}
