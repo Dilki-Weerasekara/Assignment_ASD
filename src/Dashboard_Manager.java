91a609dc5ce58cbaf89844898538a3aadc92c9b4
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-17 19:12:11 +0000

    Design the Layout of Dashboard_Manager.form

---
+++ src/Dashboard_Manager.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Dashboard_Manager {
+    private JPanel panelMain;
+    private JPanel panelButtons;
+    private JButton btn_AddRoom;
+    private JButton btn_UpdateRoom;
+    private JButton btn_ViewRoom;
+    private JButton btn_RemoveRoom;
+    private JButton btn_RemoveRoomRecords;
+    private JButton btn_AddBooking;
+    private JButton btn_UpdateBooking;
+    private JButton btn_ViewBooking;
+    private JPanel panelBackground;
+    private JLabel lbl_BackgroundImage;
+}
