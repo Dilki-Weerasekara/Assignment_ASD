3b9b4021be7b262e78d1f17b0c0c5ca57f9ea65c
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 03:25:08 +0000

    Developed User.java Class in Model package

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,4 +1,62 @@
 package Model;
 
 public class User {
+
+    //define the variables
+    int User_id;
+    String Name;
+    String Position;
+    String Email;
+    String Password;
+
+    //add Main Constructor
+    public User(int user_id, String name, String position, String email, String password) {
+        User_id = user_id;
+        Name = name;
+        Position = position;
+        Email = email;
+        Password = password;
+    }
+
+    //set Getters and Setters
+
+    public int getUser_id() {
+        return User_id;
+    }
+
+    public void setUser_id(int user_id) {
+        User_id = user_id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public void setPosition(String position) {
+        Position = position;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
