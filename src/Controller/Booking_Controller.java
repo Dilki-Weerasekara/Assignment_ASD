51a94a941eed20d0840e0ba7e34360419a5f268d
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 14:51:26 +0000

    Override crud methods in Booking_Controller.java

--- src/Controller/Booking_Controller.java
+++ src/Controller/Booking_Controller.java
@@ -1,4 +1,41 @@
 package Controller;
 
-public class Booking_Controller {
+import Model.Booking;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class Booking_Controller implements Booking_Interface {
+
+    Connection con;
+
+    //get database connection
+    public Booking_Controller() throws Exception{
+        this.con = Database.Db_Connection.getDb_Connection();
+    }
+
+    @Override
+    public void save(Booking booking) {
+
+    }
+
+    @Override
+    public void update(Booking booking) {
+
+    }
+
+    @Override
+    public void delete(Booking booking) {
+
+    }
+
+    @Override
+    public List<Booking> list() {
+        return null;
+    }
+
+    @Override
+    public Booking get(int Booking_id) {
+        return null;
+    }
 }
