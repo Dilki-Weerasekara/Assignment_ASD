c76648b188250b9bc931c9970ee86b5bbf020903
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 14:50:05 +0000

    Create Crud Operations in Booking_Interface.java

--- src/Controller/Booking_Interface.java
+++ src/Controller/Booking_Interface.java
@@ -1,4 +1,26 @@
 package Controller;
+import Model.Booking;
+
+import java.awt.print.Book;
+import java.util.List;
 
 public interface Booking_Interface {
+
+    //create crud operations
+
+    //save details
+    public void save(Booking booking);
+
+    //update details
+    public void update(Booking booking);
+
+    //delete details
+    public void delete(Booking booking);
+
+    //view all details
+    public List<Booking> list();
+
+    //search specific data
+    public Booking get(int Booking_id);
+
 }
