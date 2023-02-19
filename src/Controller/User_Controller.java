4f182d0d0df2a0f55253cefaeea8f19489ad159e
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 14:51:39 +0000

    Override crud methods in User_Controller.java

--- src/Controller/User_Controller.java
+++ src/Controller/User_Controller.java
@@ -1,5 +1,41 @@
 package Controller;
 
-public class User_Controller {
+import Model.User;
 
+import java.sql.Connection;
+import java.util.List;
+
+public class User_Controller implements User_Interface {
+
+    Connection con;
+
+    //get database connection
+    public User_Controller() throws Exception{
+        this.con = Database.Db_Connection.getDb_Connection();
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public List<User> list() {
+        return null;
+    }
+
+    @Override
+    public User get(int User_id) {
+        return null;
+    }
 }
