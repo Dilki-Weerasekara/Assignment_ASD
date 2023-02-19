41e366dd73ce9e6df07c9046484255ca9cb650fc
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 14:50:20 +0000

    Create Crud Operations in User_Interface.java

--- src/Controller/User_Interface.java
+++ src/Controller/User_Interface.java
@@ -1,4 +1,26 @@
 package Controller;
 
+import Model.User;
+import java.util.List;
+
 public interface User_Interface {
+
+    //create crud operations
+
+    //save details
+    public void save(User user);
+
+    //update details
+    public void update(User user);
+
+    //delete details
+    public void delete(User user);
+
+    //View all details
+    public List<User> list();
+
+    //search details
+    public User get(int User_id);
+
+
 }
