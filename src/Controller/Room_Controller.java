af42994a677ac0450580736711c1c768fd04fd56
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 03:47:31 +0000

    Minor Changes Done

--- src/Controller/Room_Controller.java
+++ src/Controller/Room_Controller.java
@@ -1,4 +1,14 @@
 package Controller;
+import Model.Room;
+import java.sql.Connection;
 
 public class Room_Controller {
+    Connection con;
+
+    //get database connection
+    public Room_Controller() throws Exception{
+        this.con = Database.Db_Connection.getDb_Connection();
+    }
+
+
 }
--- src/Controller/User_Controller.java
+++ src/Controller/User_Controller.java
@@ -1,4 +1,5 @@
 package Controller;
 
 public class User_Controller {
+
 }
