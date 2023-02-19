8c5c215b703558774fda05c6c055de28b2683394
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 20:00:59 +0000

    Minor Changes Done

--- src/Controller/Room_Controller.java
+++ src/Controller/Room_Controller.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.sql.Connection;
 import java.util.List;
 import java.sql.Statement;
-import java.sql.SQLException;
+
 
 public class Room_Controller implements Room_Interface{
     Connection con;
