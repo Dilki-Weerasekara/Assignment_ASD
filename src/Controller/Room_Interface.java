ea8d54df283d264427ac4423c71f28c46c361aa9
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 04:12:53 +0000

    Developed Room_Interface.java

--- src/Controller/Room_Interface.java
+++ src/Controller/Room_Interface.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import Model.Room;
+
+import java.util.List;
+
 public interface Room_Interface {
+    public void save (Room room);
+    public void update (Room room);
+    public void delete(Room room);
+    public List<Room> list();
+    public Room get(int roomNo);
 }
