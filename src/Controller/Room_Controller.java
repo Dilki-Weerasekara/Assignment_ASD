616c2362c99f803b127273b55016aeb61420c42f
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-02-19 16:29:19 +0000

    Save and Update Crud Operations Done

--- src/Controller/Room_Controller.java
+++ src/Controller/Room_Controller.java
@@ -1,7 +1,11 @@
 package Controller;
 import Model.Room;
+
+import javax.swing.*;
 import java.sql.Connection;
 import java.util.List;
+import java.sql.Statement;
+import java.sql.SQLException;
 
 public class Room_Controller implements Room_Interface{
     Connection con;
@@ -12,12 +16,50 @@ public class Room_Controller implements Room_Interface{
     }
 
     @Override
-    public void save(Room room) {
+    public void save(Room room){
+        
+        try{
+            //insert query
+            String query = "INSERT INTO room(RoomNo, Type, Size, Status, Reason, FromDate, ToDate, StartTime, EndTime) " +
+                    "VALUES ('"+room.getRoomNo()+"','"+room.getType()+"','"+room.getSize()+"','"+room.getStatus()+"'," +
+                    "'"+room.getReason()+"','"+room.getFromDate()+"','"+room.getToDate()+"','"+room.getStartTime()+"','"+room.getEndTime()+"')";
+
+            //set query
+            Statement statement = con.createStatement();
+
+            //execute query
+            statement.execute(query);
+
+            //Show message dialog
+            JOptionPane.showMessageDialog(null,"Successfully Saved","information",JOptionPane.INFORMATION_MESSAGE);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
 
     }
 
     @Override
     public void update(Room room) {
+
+        try{
+            //update query
+            String query = "UPDATE `room` SET `RoomNo`='"+room.getRoomNo()+"',`Type`='"+room.getType()+"',`Size`='"+room.getSize()+"',`Status`='"+room.getStatus()+"'," +
+                    "`Reason`='"+room.getReason()+"',`FromDate`='"+room.getFromDate()+"',`ToDate`='"+room.getToDate()+"',`StartTime`='"+room.getStartTime()+"'," +
+                    "`EndTime`='"+room.getEndTime()+"' WHERE RoomNo = '"+room.getRoomNo()+"'";
+
+            //set query
+            Statement statement = con.createStatement();
+
+            //execute query
+            statement.execute(query);
+
+            //Show message dialog
+            JOptionPane.showMessageDialog(null,"Successfully Updated","information",JOptionPane.INFORMATION_MESSAGE);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
