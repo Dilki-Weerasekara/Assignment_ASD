9ff3bb623dd8ddc6899a1c2329bdfca077c6623e
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 14:17:02 +0000

    Make a view of GUI RemoveRoom_Manager.java and Cancel button

--- src/View/RemoveRoom_Manager.java
+++ src/View/RemoveRoom_Manager.java
@@ -1,8 +1,11 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RemoveRoom_Manager {
+public class RemoveRoom_Manager extends JFrame{
     private JPanel panelMain;
     private JLabel lbl_RemoveRoom;
     private JLabel lbl_RoomNo;
@@ -11,4 +14,24 @@ public class RemoveRoom_Manager {
     private JTextArea txt_Reason;
     private JButton btn_Delete;
     private JButton btn_Cancel;
+
+    public RemoveRoom_Manager(){
+        super();
+        setTitle("Remove Room Form");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(600,400));
+        setResizable(false);
+        btn_Cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        //create a new object
+        RemoveRoom_Manager removeRoom_manager = new RemoveRoom_Manager();
+        removeRoom_manager.setVisible(true);
+    }
 }
