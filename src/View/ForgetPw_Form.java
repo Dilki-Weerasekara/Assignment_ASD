f4985252649b3c82e16cca433f0a6a76c6a7ddf2
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 14:54:16 +0000

    Make a View of GUI ForgetPw_Form.java

--- src/View/ForgetPw_Form.java
+++ src/View/ForgetPw_Form.java
@@ -1,8 +1,11 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ForgetPw_Form {
+public class ForgetPw_Form extends JFrame{
     private JPanel panelMain;
     private JLabel lbl_forgetPw;
     private JLabel lbl_Email;
@@ -13,4 +16,26 @@ public class ForgetPw_Form {
     private JTextField txt_Email;
     private JPasswordField txt_NewPw;
     private JLabel lbl_NewPw;
+
+    //constructor
+    public ForgetPw_Form(){
+        super();
+        setTitle("Forget Password Form");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(600,400));
+        setResizable(false);
+        btn_cansel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        //create a new object
+        ForgetPw_Form forgetPw_form = new ForgetPw_Form();
+        forgetPw_form.setVisible(true);
+    }
 }
