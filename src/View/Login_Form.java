f1111d8b2c39e30845ca9aefaf4d88183f8cd73e
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 15:01:19 +0000

    Make a View of GUI Login_Form.java

--- src/View/Login_Form.java
+++ src/View/Login_Form.java
@@ -1,8 +1,11 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login_Form {
+public class Login_Form extends JFrame{
 
     private JLabel lbl_Login;
 
@@ -15,6 +18,25 @@ public class Login_Form {
         private JPasswordField txt_password;
         private JLabel lbl_LogoIcon;
         private JButton btn_Logic;
+
+        //constructor
+    public Login_Form(){
+        super();
+        setTitle("Login Form");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(600,400));
+        setResizable(false);
+        btn_cansel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+    public static void main(String[] args) {
+        Login_Form login_form = new Login_Form();
+        login_form.setVisible(true);
+    }
 
     }
 
