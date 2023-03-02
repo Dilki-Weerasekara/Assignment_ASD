2800a2ba7af83bfbde3a4ed7b7926a08a1f953ce
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 14:22:10 +0000

    Make a View of GUI RemoveRoom_Details.java

--- src/View/RemoveRoom_Details.java
+++ src/View/RemoveRoom_Details.java
@@ -1,11 +1,28 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class RemoveRoom_Details {
+public class RemoveRoom_Details extends JFrame{
     private JPanel panelMain;
     private JLabel lbl_RemoveRoomDetails;
     private JTextField txt_Search;
     private JLabel lbl_SearchIcon;
     private JTable tb_RemoveRoom;
+
+    //constructor
+    public RemoveRoom_Details(){
+        super();
+        setTitle("Remove Room View Details");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(800,400));
+        setResizable(false);
+
+    }
+
+    public static void main(String[] args) {
+        RemoveRoom_Details removeRoomDetails = new RemoveRoom_Details();
+        removeRoomDetails.setVisible(true);
+    }
+
 }
