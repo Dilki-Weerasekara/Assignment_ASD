cecbabc1c9a271bb79ab084a31ae5d4e1948511c
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 18:13:56 +0000

    Save Room Details and Add Concurrency using Threads

--- src/View/AddRoom_Manager.java
+++ src/View/AddRoom_Manager.java
@@ -1,9 +1,16 @@
 package View;
 
+import Controller.Room_Controller;
+import Controller.User_Controller;
+import Model.Room;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AddRoom_Manager extends JFrame{
     private JPanel panelMain;
@@ -15,9 +22,6 @@ public class AddRoom_Manager extends JFrame{
     private JButton btn_Cancel;
     private JTextField txt_RoomNo;
     private JTextField txt_RoomSize;
-    private JRadioButton btn_radioComputLab;
-    private JRadioButton btn_radioTutorial;
-    private JRadioButton btn_radioLecture_Theatre;
     private JLabel lbl_CurrentStatus;
     private JComboBox comboBox_status;
     private JLabel lbl_Reason;
@@ -32,6 +36,7 @@ public class AddRoom_Manager extends JFrame{
     private JTextField txt_End;
     private JLabel lbl_End;
     private JLabel lbl_Start;
+    private JComboBox comboBox_Type;
 
     //constructor
     public AddRoom_Manager(){
@@ -48,6 +53,59 @@ public class AddRoom_Manager extends JFrame{
             }
         });
 
+        btn_Save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //create an object from Model Class
+                Room room = new Room();
+
+                //concurrency
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+                executorService.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            //get values
+                            String roomNo = txt_RoomNo.getText().toString();
+                            String roomType = comboBox_Type.getSelectedItem().toString();
+                            int roomSize = Integer.parseInt(txt_RoomSize.getText());
+                            String currentStatus = comboBox_status.getSelectedItem().toString();
+                            String reason = txt_Reason.getText().toString();
+
+                            //Date and Time get
+
+
+
+                            room.setRoomNo(roomNo);
+                            room.setType(roomType);
+                            room.setSize(roomSize);
+                            room.setStatus(currentStatus);
+                            room.setReason(reason);
+
+                            //Date and Time set
+
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                });
+                executorService.shutdown();
+
+                try {
+                    //create new object
+                    Room_Controller room_controller = new Room_Controller();
+                    room_controller.save(room);
+
+                    //close the current UI
+                    dispose();
+
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
