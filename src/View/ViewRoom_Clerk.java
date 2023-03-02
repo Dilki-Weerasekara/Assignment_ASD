6d8c075d34e7b32646f000a271ee217db135ade2
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 14:08:25 +0000

    Make a view of GUI ViewRoom_Clerk.java

--- src/View/ViewRoom_Clerk.java
+++ src/View/ViewRoom_Clerk.java
@@ -1,11 +1,29 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class ViewRoom_Clerk {
+public class ViewRoom_Clerk extends JFrame{
     private JLabel lbl_ViewRoom;
     private JPanel panelMain;
     private JTextField txt_Search;
     private JLabel lbl_Search;
     private JTable tb_ViewRoomDetails;
+
+
+    //constructor
+    public ViewRoom_Clerk(){
+        super();
+        setTitle("View Room Clerk");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(800,400));
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+
+        //create a new object
+        ViewRoom_Clerk viewRoom_clerk = new ViewRoom_Clerk();
+        viewRoom_clerk.setVisible(true);
+    }
 }
