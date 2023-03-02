5d32d01e46112033e1c6713a43c85444b9ec646a
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 14:41:07 +0000

    Make a View of GUI Dashboard_Clerk.java

--- src/View/Dashboard_Clerk.java
+++ src/View/Dashboard_Clerk.java
@@ -1,8 +1,9 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Dashboard_Clerk {
+public class Dashboard_Clerk extends JFrame{
     private JPanel panelMain;
     private JLabel lbl_DashboardClerk;
     private JButton btn_ViewRooms;
@@ -10,4 +11,21 @@ public class Dashboard_Clerk {
     private JButton btn_ViewBooking;
     private JPanel panelButtons;
     private JLabel lbl_BackgroundImage;
+
+
+    //constructor
+    public Dashboard_Clerk(){
+        super();
+        setTitle("Add Room Form");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(2000,800));
+        setResizable(false);
+    }
+
+
+    public static void main(String[] args) {
+        //create a new object
+        Dashboard_Clerk dashboard_clerk = new Dashboard_Clerk();
+        dashboard_clerk.setVisible(true);
+    }
 }
