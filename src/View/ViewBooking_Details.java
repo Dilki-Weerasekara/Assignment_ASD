e45c5d5158a9cbb288380adaf27f7acbd0a73f16
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-03-02 14:33:31 +0000

    Make a View of GUI ViewBooking_Details.java

--- src/View/ViewBooking_Details.java
+++ src/View/ViewBooking_Details.java
@@ -1,10 +1,26 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class ViewBooking_Details {
+public class ViewBooking_Details extends JFrame{
     private JPanel panelMain;
     private JTextField txtSearch;
     private JTable tb_ViewBookingDetails;
     private JLabel lbl_SerachIcon;
+
+    //constructor
+    public ViewBooking_Details(){
+        super();
+        setTitle("View Booking Details");
+        setContentPane(panelMain);
+        setMinimumSize(new Dimension(800,400));
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        //create a new object
+        ViewBooking_Details viewBookingDetails = new ViewBooking_Details();
+        viewBookingDetails.setVisible(true);
+    }
 }
